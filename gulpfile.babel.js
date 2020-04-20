57de3b769d0956ba7b91a9eb0fc5b7e782b2ae1f
aaaaa <amikusanta@gmail.com>
2020-04-20 18:06:54 +0900

    ファイズフォンバージョン１完成

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,79 +1,3 @@
-// import gulp  from 'gulp';
-// import webpack from 'webpack-stream';
-// import webpackConfig from './webpack.config.js';
-// import sass from 'gulp-sass';
-// import browserSync from 'browser-sync';
-// import notify from 'gulp-notify';
-// import plumber from 'gulp-plumber';
-// import eslint from 'gulp-eslint';
-//
-//
-// //sassファイルをcssに
-// gulp.task('sass', function () {
-//     return gulp.src('./scss/**/*.scss')
-//         .pipe(sass().on('error', sass.logError))
-//         .pipe(gulp.dest('./src/css'));
-// });
-//
-// //変化したら自動でlord
-// gulp.task('browser-sync', function() {
-//     browserSync.init({
-//         server: {
-//             baseDir: "./"
-//         }
-//     });
-// });
-//
-// gulp.task('bs-reload',function(){
-//     browserSync.reload();
-// });
-//
-// gulp.task('eslint',function(){
-//     return gulp.src(['src/**/*.js']) //lintのチェック先を指定
-//         .pipe(plumber({
-//             //エラーをハンドル
-//             errorHandler:function(error){
-//                 const taskName ='eslint';
-//                 const title = '[task]' + taskName + ' ' + error.plugin;
-//                 const errorMsg= 'error:' + error.message;
-//                 //ターミナルにエラーを出力
-//                 console.log(title + '`\n' + errorMsg);
-//                 //エラーを通知
-//                 notify.onError({
-//                     title:title,
-//                     message:errorMsg,
-//                     time:3000
-//                 });
-//             }
-//         }))
-//         .pipe(eslint({ useEslintrc:true})) //eslintrcを参照
-//         .pipe(eslint.format())
-//         .pipe(eslint.failOnError())
-//         .pipe(plumber.stop());
-// });
-//
-// gulp.task('build', function () {
-//     return gulp.src('src/js/*.js')
-//         .pipe(plumber({
-//             errorHandler: notify.onError("Error: <%= error.message %>")
-//         }))
-//         .pipe(webpack(webpackConfig))
-//         .pipe(gulp.dest("dist/js"));
-//
-// });
-
-
-// gulp.task('default',['sass','browser-sync','eslint','build','bs-reload'],function () {
-//     gulp.watch('/scss/**/*.scss',['sass']);
-//     gulp.watch('./src/*/*.js',['build']);
-//     gulp.watch("./*.html",['bs-reload']);
-//     gulp.watch("./dist/*/*.+(js|css)",['bs-reload']);
-//     gulp.watch("./src/css/*.css",['bs-reload']);
-//     gulp.watch("./src/**/*.js",['eslint']);
-// })
-
-// import 定数宣言　名前（なんでもいい）　from ライブラリのモジュールを選択 書き方は最後のやつをぐぐれ
-
 
 
 
@@ -143,7 +67,7 @@ gulp.task('eslint',function(){
                 });
             }
         }))
-        .pipe(eslint({ useEslintrc:true})) //eslintrcを参照
+        .pipe(eslint({ useEslintrc:true}))
         .pipe(eslint.format())
         .pipe(eslint.failOnError())
         .pipe(plumber.stop());
---
+++ src/js/music.js
@@ -0,0 +1,223 @@
+import Vue from 'vue';
+
+new Vue({
+    el: '#header',
+    data: {
+        current: {},
+        count: 0,
+        hensin:false,
+        number: '',
+        single:false,
+        burst:false,
+        shotcount:12,
+        standby: false,
+        player: new Audio(),
+        se: [
+            {
+                id: 1,
+                title: 'faizop',
+                src: './music/fiazop.mp3'
+            },
+            {
+                id: 2,
+                title: 'faiz1',
+                src: './music/faizp1.mp3'
+            },
+            {
+                id: 3,
+                title: 'faiz2',
+                src: './music/faizp2.mp3'
+            },
+            {
+                id: 4,
+                title: 'faiz3',
+                src: './music/faizp3.mp3'
+            },
+            {
+                id: 5,
+                title: 'standoby',
+                src: './music/standoby2.mp3'
+            },
+            {
+                id: 6,
+                title: 'hensin',
+                src: './music/setcomplete.mp3'
+            },
+            {
+                id: 7,
+                title: 'Exceed',
+                src: './music/Exceed.mp3'
+            },
+            {
+                id: 8,
+                title: 'jetStrikerComeCloser',
+                src: './music/3821.mp3'
+            },
+            {
+                id: 9,
+                title: 'error',
+                src: './music/error.mp3'
+            },
+            {
+                id: 10,
+                title: 'singleMode',
+                src: './music/singmode.mp3'
+            },
+            {
+                id: 11,
+                title: 'burstMode',
+                src: './music/burstmode.mp3'
+            },
+            {
+                id: 12,
+                title: 'charge',
+                src: './music/charge.mp3'
+            },
+            {
+                id: 13,
+                title: 'off',
+                src: './music/off.mp3'
+            },
+            {
+                id: 14,
+                title: 'singelModeSe',
+                src: './music/singlese.mp3'
+            },
+            {
+                id: 15,
+                title: 'burstModeSe',
+                src: './music/burstse.mp3'
+            },
+            {
+                id: 16,
+                title: 'null',
+                src: './music/null.mp3'
+            },
+
+        ]
+    },
+    //起動音
+    // mounted: function () {
+    //     this.current = this.se[0];
+    //     this.player.src = this.current.src;
+    //     this.player.play();
+    // },
+    methods: {
+        playBtn: function (num) {
+            if (this.count === 0) {
+                this.current = this.se[1];
+                this.player.src = this.current.src;
+                this.player.play();
+                this.number = num;
+                this.count++;
+                console.log(this.number);
+            } else if (this.count === 1) {
+                this.current = this.se[2];
+                this.player.src = this.current.src;
+                this.player.play();
+                 this.number = this.number + num;
+                this.count++;
+                console.log(this.number);
+            } else if (this.count === 2) {
+                this.current = this.se[3];
+                this.player.src = this.current.src;
+                this.player.play();
+                 this.number = this.number + num;
+                this.count++;
+                console.log(this.number);
+            }
+            else if (this.count === 3) {
+                this.current = this.se[3];
+                this.player.src = this.current.src;
+                this.player.play();
+                this.number = this.number + num;
+                this.count = 0;
+                console.log(this.number);
+            }
+        },
+
+
+
+        judg: function () {
+            if (this.number === '555' && this.hensin === false) {
+                //変身
+                this.current = this.se[4];
+                this.hensin = true;
+                setTimeout(function () {
+                    this.current = this.se[5];
+                    this.player.src = this.current.src;
+                    this.player.play();
+                    this.number = '';
+               }.bind(this), 1100)
+            }else if(this.hensin !== false && this.number ===''){
+                //Exceed charge
+                this.current = this.se[6];
+            }else if(this.hensin !== false && this.number === '3821'){
+                this.current = this.se[7];
+            }else if(this.hensin !== false && this.number === '103'){
+                this.current = this.se[9];
+                this.single = true;
+                this.burst = false;
+            }else if(this.hensin !== false && this.number === '106'){
+                this.current = this.se[10];
+                this.burst = true;
+                this.single =false;
+            }else if(this.hensin !== false && this.number === '279'){
+                this.current = this.se[11];
+                this.shotcount = 12;
+            }else{
+                //指定の番号出なければエラー音
+                this.current = this.se[8];
+                this.player.src = this.current.src;
+                this.player.play();
+            }
+            this.player.src = this.current.src;
+            this.player.play();
+            //番号の初期化
+            this.number = '';
+            this.count =0;
+        },
+        //変身解除
+        off:function(){
+            if(this.hensin === true){
+                this.current = this.se[12];
+                this.player.src = this.current.src;
+                this.player.play();
+                this.hensin = false
+            }
+        },
+        //コマンド初期化
+        clear:function () {
+            this.number = '';
+            this.count =0;
+        },
+        shot:function(){
+            if(this.single === true && this.hensin === true){
+                this.current = this.se[13];
+                this.player.src = this.current.src;
+                this.player.play();
+                this.shotcount--;
+                if(this.shotcount < 0){
+                    this.current = this.se[15];
+                    this.player.src = this.current.src;
+                    this.player.play();
+                }
+            }else if(this.burst === true && this.hensin === true){
+                this.current = this.se[14];
+                this.player.src = this.current.src;
+                this.player.play();
+                this.shotcount = this.shotcount-3;
+                console.log(this.shotcount);
+                if(this.shotcount < 0){
+                    this.current = this.se[15];
+                    this.player.src = this.current.src;
+                    this.player.play();
+                }
+            }
+        }
+
+
+    },
+
+
+})
